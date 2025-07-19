3128f7e44db7120e5c0d5b2cd4957891e99dac01
BoegyNat <bboegy@gmail.com>
2025-07-19 09:36:16 +0700

    Refactor code

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-print("hello")
-
-print("hi My name is Boegy")
-
-print("hi my name is Atom")
-
-print("Auto: Hello!")
-
-
+# Configuration Section
 import turtle as t
 
 t.speed(1)
@@ -16,78 +8,72 @@ t.width(5)
 t.color("#ff0000")
 
 
-
-def drawSquare(length):
-    t.left(90)
-    for _ in range(4):
-        t.forward(100)
-        t.left(360/4)
-
+# Function shape Section
 def drawRectangle(width, length):
     for _ in range(2):        
-        t.forward(50)
+        t.forward(width)
         t.left(360/4)
-        t.forward(100)  
+        t.forward(length)  
         t.left(360/4)
 
 def drawTriangle(length):
     for _ in range(3):
-        t.forward(100)
+        t.forward(length)
         t.right(360/3)
 
 def drawCircle(length):
     for _ in range(360):        
-        t.forward(10)
+        t.forward(length)
         t.left(360/360)
 
 def drawPentagon(length):
     for _ in range(5):
-        t.forward(100)
+        t.forward(length)
         t.left(360/5)
 
-def drawHexagon(length):
-    for _ in range(6):
-        t.forward(100)
-        t.left(360/6)
+def drawHexagon(lenght):
+    t.forward(lenght)
+    t.left(60)
+    t.forward(lenght)
+    t.left(60)
+    t.forward(lenght)
+    t.left(60)
+    t.forward(lenght)
+    t.left(60)
+    t.forward(lenght)
+    t.left(60)
+    t.forward(lenght)
 
+def drawSquare(lenght):
+    t.forward(lenght)
+    t.left(90)
+    t.forward(lenght)
+    t.left(90)
+    t.forward(lenght)
+    t.left(90)
+    t.forward(lenght)
+
+def Goto(x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+# Function to draw any thing
 def drawHouse():
     drawSquare(4)
     t.forward(100)
     t.left(90)
     drawTriangle(3)
 
+# def drawGirl():
 
-# drawHouse()
-# drawHexagon(100)
 
-t. speed(10)
-
-def draw_hexagon(lenght):
-    t.forward(lenght)
-    t.left(60)
-    t.forward(lenght)
-    t.left(60)
-    t.forward(lenght)
-    t.left(60)
-    t.forward(lenght)
-    t.left(60)
-    t.forward(lenght)
-    t.left(60)
-    t.forward(lenght)
-def draw_square(lenght):
-    t.forward(lenght)
-    t.left(90)
-    t.forward(lenght)
-    t.left(90)
-    t.forward(lenght)
-    t.left(90)
-    t.forward(lenght)
+# Main Section
 drawHouse()
 t.right(180)
 t.penup()
 t.forward(200)
 t.pendown()
-draw_hexagon(50)
-drawRectangle(50)
-# draw_square(50)
+drawHexagon(50)
+
 t.done()

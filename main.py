44c3dfe85086257f0662d2ef2f93404e20bf63ef
Tomato <atomkung88@gmail.com>
2025-07-19 10:32:51 +0700

    draw tree

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Configuration Section
 import turtle as t
 
-t.speed(1)
+t.speed(1000)
 
 t.width(5)
     
@@ -9,6 +9,11 @@ t.color("#ff0000")
 
 
 # Function shape Section
+def drawSquare(length):
+ for i in range(4):
+    t.forward(length)
+    t.left(90)
+
 def drawRectangle(width, length):
     for _ in range(2):        
         t.forward(width)
@@ -36,11 +41,6 @@ def drawHexagon(length):
     t.forward(length)
     t.left(60)
 
-def drawSquare(length):
- for i in range(4):
-    t.forward(length)
-    t.left(90)
-
 def Goto(x, y):
     t.penup()
     t.goto(x, y)
@@ -50,16 +50,30 @@ def Goto(x, y):
 def drawHouse():
     t.left(90)
     drawSquare(100)
-    t.left(90)
+    # t.left(90)
     t.forward(100)
     t.left(90)
     drawTriangle(100)
+
+def drawTree():
+    t.left(180)    
+    drawRectangle(50, 100)
+    t.left(90)  
+    t.forward(100)
+    t.right(90)
+    t.backward(25)
+    drawPentagon(100)
+
+
+
 
 # def drawGirl():
 
 
 # Main Section
 drawHouse()
+Goto(300, 0)
+drawTree()
 # t.right(180)
 # t.penup()
 # t.forward(200)
